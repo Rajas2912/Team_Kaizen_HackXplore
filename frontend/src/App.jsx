288b9a3380f8d5bfae9a0311480502038b8184da
Prathmesh Phatake <prathmeshPhatake4@gmail.com>
2025-02-12 03:02:32 +0530

    added main my branch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,36 @@
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './common/Navbar.jsx';
 import Sidebar from './common/Sidebar.jsx';
 import Home from './pagesKM/Home/Home.jsx';
 import Login from './pagesPP/login.jsx';
 import RegisterForm from './pagesPP/Register.jsx';
 
+function Layout({ children }) {
+  const location = useLocation();
+  const isAuthPage = location.pathname === '/' || location.pathname === '/register';
+
+  return (
+    <>
+      {!isAuthPage && <Navbar />}
+      {!isAuthPage && <Sidebar />}
+      {children}
+    </>
+  );
+}
+
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
-      <Sidebar />
-      <Routes>
-        <Route path="/home" element={<Home />}></Route>
-        <Route path="/" element={<Login />}></Route>
-        <Route path="/register" element={<RegisterForm />}></Route>
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<RegisterForm />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
